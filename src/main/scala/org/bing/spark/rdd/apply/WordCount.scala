a1ed3d1b9a6ab9f8b3d3a5ad20e973045f54a36e
shuiliuxing <1270129994@qq.com>
2020-11-28 18:42:00 +0800

    【修改】统计每个元素的个数

---
+++ src/main/scala/org/bing/spark/rdd/apply/WordCount.scala
@@ -0,0 +1,26 @@
+package org.bing.spark.rdd.apply
+
+import org.apache.spark.{SparkConf, SparkContext}
+/*
+  需求：统计每个元素的个数
+  文本：
+    a , b , c , d
+
+    b , b , f , e
+
+    a , a , c , f
+ */
+object WordCount {
+  def main(args: Array[String]): Unit = {
+    val conf=new SparkConf().setAppName("WordCount Test").setMaster("local")
+    val sc=new SparkContext(conf)
+
+    val rdd=sc.textFile("E:\\data\\spark\\rdd\\test\\read\\wordCount.txt")
+      .filter(!_.isEmpty)
+      .flatMap(_.split(","))
+      .map(x=>(x.trim,1))
+      .reduceByKey(_+_)
+      .sortByKey(true)
+    rdd.foreach(println)
+  }
+}
